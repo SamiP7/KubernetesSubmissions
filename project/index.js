54d1a7b15fb29b14493e65a54359e8eb9a346b2b
SamiP7 <sami.parkkinenn@gmail.com>
2025-12-13 13:59:47 +0200

    todo pictures

--- project/index.js
+++ project/index.js
@@ -1,13 +1,42 @@
 const express = require('express')
 const app = express()
-
-
 const port = process.env.PORT || 8000
+const fs = require('fs');
+const {setInterval} = require('timers');
+const axios = require('axios');
+const path = require('path');
+
+async function download() {
+    
+    
+    const dir = path.resolve(__dirname, './templates/', 'image.jpg')
+    const url = 'https://picsum.photos/200'
+    axios({url: url, responseType: 'stream', method: 'GET' }).then(res => {
+    res.data.pipe(fs.createWriteStream(dir));
+    
+    res.data.on("end", () => {
+      console.log("download complete");
+    });
+  });
+    
+    
+    console.log('New image will be displayed')
+}
+
+download()
+
+setInterval(() => {
+    
+    download()
+}, 600000)
+
+app.use(express.static('templates'));
 
 app.get('/', (req, res) => {
-    res.send('Server is running!')
+    
+    res.sendFile('/index.html');
 })
 
 app.listen(port, () => {
-    console.log(`Server started in port ${port}`)
+    console.log(`Server started in port ${port}`);
 })
