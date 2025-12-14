68460744d57486ca9fca1aeb83c30540f8b481a6
SamiP7 <sami.parkkinenn@gmail.com>
2025-12-14 14:50:39 +0200

    persistentvolume added

--- project/index.js
+++ project/index.js
@@ -6,35 +6,59 @@ const {setInterval} = require('timers');
 const axios = require('axios');
 const path = require('path');
 
+const imgdir = path.resolve(__dirname, '/usr/src/app/files/', 'image.jpg')
+const dir = path.resolve(__dirname, '/usr/src/app/files/', 'date.json')
+
 async function download() {
-    
-    
-    const dir = path.resolve(__dirname, './templates/', 'image.jpg')
     const url = 'https://picsum.photos/200'
     axios({url: url, responseType: 'stream', method: 'GET' }).then(res => {
-    res.data.pipe(fs.createWriteStream(dir));
+    res.data.pipe(fs.createWriteStream(imgdir));
     
     res.data.on("end", () => {
       console.log("download complete");
-    });
-  });
-    
+    })
+  })
     
     console.log('New image will be displayed')
 }
 
-download()
+async function datecomp(time) {
+  if (fs.existsSync(dir) && fs.existsSync(imgdir)) {
+    var jsondata = fs.readFileSync(dir)
+    diff = (Date.now() - JSON.parse(jsondata)) / 1000
+    if (diff >= 600) {
+      fs.writeFile(dir, JSON.stringify(Date.now()), function(err) {
+        if (err) {
+          console.log(err)
+        }
+        console.log('time found and new written')
+        download()
+      })
+    }
+  } else {
+    var jsondata = JSON.stringify(time)
+    fs.writeFile(dir, jsondata, function(err) {
+      if (err) {
+        console.log(err)
+      }
+      download()
+      console.log('time written')
+    })
+  }
+    
+}
+
+datecomp(Date.now())
 
 setInterval(() => {
-    
-    download()
-}, 600000)
+    datecomp(Date.now())
+}, 30000)
 
-app.use(express.static('templates'));
+app.use(express.static('../'))
 
 app.get('/', (req, res) => {
-    
-    res.sendFile('/index.html');
+    res.sendFile(__dirname + '/templates/index.html');
+
 })
 
 app.listen(port, () => {
